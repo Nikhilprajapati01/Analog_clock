fa6844cde67e2fe3797b63afaaa4e496c4fca4b5
nikhil_kumar <nkman2002@gmail.com>
2024-02-27 23:06:36 +0530

    date

---
+++ app.js
@@ -0,0 +1,21 @@
+let hrs = document.getElementById('hour')
+let min = document.getElementById('min')
+let sec = document.getElementById('sec')
+
+function displaytime (){
+    const D = new Date;
+
+    let hh =  D.getHours();
+    let mm =  D.getMinutes();
+    let ss =  D.getSeconds();
+
+   hrs.style.transform = `rotate(${hh * 30}deg)`
+   min.style.transform = `rotate(${mm * 6}deg)`
+   sec.style.transform = `rotate(${ss * 6}deg)`
+}
+
+
+
+setInterval(displaytime,1000)
+
+localStorage()
